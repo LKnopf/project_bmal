96a4e832959f4f49b6cbdcf55ea2f077407f9bcf
Lars Knopf <knopf@born.itb.pri>
2017-06-08 15:58:37 +0200

    changed to split

--- script5_mergeReplicates.sh
+++ script5_mergeReplicates.sh
@@ -3,16 +3,16 @@
 
 
 
-mergePeaks -d given 150 ZT02_r1_bw_peaks.bed ZT02_r2_bw_peaks.bed > ZT02_C.bed
-mergePeaks -d given 150 ZT06_r1_bw_peaks.bed ZT06_r2_bw_peaks.bed > ZT06_C.bed
-mergePeaks -d given 150 ZT10_r1_bw_peaks.bed ZT10_r2_bw_peaks.bed > ZT10_C.bed
-mergePeaks -d given 150 ZT14_r1_bw_peaks.bed ZT14_r2_bw_peaks.bed > ZT14_C.bed
-mergePeaks -d given 150 ZT18_r1_bw_peaks.bed ZT18_r2_bw_peaks.bed > ZT18_C.bed
-mergePeaks -d given 150 ZT22_r1_bw_peaks.bed ZT22_r2_bw_peaks.bed > ZT22_C.bed
+mergePeaks -d given 150 ZT02_r1_bw_split_peaks.bed ZT02_r2_bw_split_peaks.bed > ZT02_split_C.bed
+mergePeaks -d given 150 ZT06_r1_bw_split_peaks.bed ZT06_r2_bw_split_peaks.bed > ZT06_split_C.bed
+mergePeaks -d given 150 ZT10_r1_bw_split_peaks.bed ZT10_r2_bw_split_peaks.bed > ZT10_split_C.bed
+mergePeaks -d given 150 ZT14_r1_bw_split_peaks.bed ZT14_r2_bw_split_peaks.bed > ZT14_split_C.bed
+mergePeaks -d given 150 ZT18_r1_bw_split_peaks.bed ZT18_r2_bw_split_peaks.bed > ZT18_split_C.bed
+mergePeaks -d given 150 ZT22_r1_bw_split_peaks.bed ZT22_r2_bw_split_peaks.bed > ZT22_split_C.bed
 
 #hier manuell sites rausnehmen, die nur 1x auftauchen!
 
-mergePeaks -d 150 ZT02_C.bed ZT06_C.bed ZT10_C.bed ZT14_C.bed ZT18_C.bed ZT22_C.bed > allmerged_C.txt
+#mergePeaks -d 150 ZT02_C.bed ZT06_C.bed ZT10_C.bed ZT14_C.bed ZT18_C.bed ZT22_C.bed > allmerged_C.txt
 
 #hier auch manuell
 
