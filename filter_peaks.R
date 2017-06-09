125d4b6a08e6cb8950f2478184e5bf460cb6a51b
Lars Knopf <knopf@born.itb.pri>
2017-06-09 17:32:26 +0200

    added r script to make allmerged file

---
+++ filter_peaks.R
@@ -0,0 +1,28 @@
+
+
+name = "ALL_split_C.bed"
+cutoff = 2 #number of total appearances a peak has to have
+merged_peaks = read.table(name, sep = "\t")
+true_peaks = subset(merged_peaks, V8 >= cutoff)
+
+
+
+"sort peaks by position and chromosome"
+chrOrder = c("chr1", "chr1_random", "chr2", "chr3", "chr4", "chr4_random", "chr5", 
+             "chr6", "chr7", "chr8", "chr8_random",  "chr9", "chr9_random", "chr10", "chr11", "chr12", "chr13", "chr13_random",
+             "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chrM", "chrUn_random", "chrX", "chrX_random", "chrY", "chrY_random")
+
+true_peaks = true_peaks[ order(true_peaks$"V3", decreasing = F), ]
+true_peaks$"V2" <- factor(true_peaks$"V2", chrOrder, ordered=TRUE)
+true_peaks = true_peaks[mixedorder(true_peaks$"V2"),]
+
+
+write.table(true_peaks[ -1 , 2:4 ], file = "allmerged_C_sub.txt", append = FALSE, quote = FALSE, sep = "\t",
+            #eol = "\n", na = "NA", dec = ".", 
+            row.names = FALSE,
+            col.names = FALSE)
+
+
+
+
+
--- script5_mergeReplicates.sh
+++ script5_mergeReplicates.sh
@@ -2,13 +2,25 @@
 
 
 
-
+#for normal peaks
 mergePeaks -d given 150 ZT02_r1_bw_split_peaks.bed ZT02_r2_bw_split_peaks.bed > ZT02_split_C.bed
 mergePeaks -d given 150 ZT06_r1_bw_split_peaks.bed ZT06_r2_bw_split_peaks.bed > ZT06_split_C.bed
 mergePeaks -d given 150 ZT10_r1_bw_split_peaks.bed ZT10_r2_bw_split_peaks.bed > ZT10_split_C.bed
 mergePeaks -d given 150 ZT14_r1_bw_split_peaks.bed ZT14_r2_bw_split_peaks.bed > ZT14_split_C.bed
 mergePeaks -d given 150 ZT18_r1_bw_split_peaks.bed ZT18_r2_bw_split_peaks.bed > ZT18_split_C.bed
 mergePeaks -d given 150 ZT22_r1_bw_split_peaks.bed ZT22_r2_bw_split_peaks.bed > ZT22_split_C.bed
+
+#for subpeaks
+
+mergePeaks -d given 150 ZT02_r1_bw_split_peaks.subpeaks.bed ZT02_r2_bw_split_peaks.subpeaks.bed > ZT02_split_C.subpeaks.bed
+mergePeaks -d given 150 ZT06_r1_bw_split_peaks.subpeaks.bed ZT06_r2_bw_split_peaks.subpeaks.bed > ZT06_split_C.subpeaks.bed
+mergePeaks -d given 150 ZT10_r1_bw_split_peaks.subpeaks.bed ZT10_r2_bw_split_peaks.subpeaks.bed > ZT10_split_C.subpeaks.bed
+mergePeaks -d given 150 ZT14_r1_bw_split_peaks.subpeaks.bed ZT14_r2_bw_split_peaks.subpeaks.bed > ZT14_split_C.subpeaks.bed
+mergePeaks -d given 150 ZT18_r1_bw_split_peaks.subpeaks.bed ZT18_r2_bw_split_peaks.subpeaks.bed > ZT18_split_C.subpeaks.bed
+mergePeaks -d given 150 ZT22_r1_bw_split_peaks.subpeaks.bed ZT22_r2_bw_split_peaks.subpeaks.bed > ZT22_split_C.subpeaks.bed
+
+#merge complete set
+mergePeaks -d given 150 ZT02_r1_bw_split_peaks.bed ZT02_r2_bw_split_peaks.bed ZT06_r1_bw_split_peaks.bed ZT06_r2_bw_split_peaks.bed > ZT06_split_C.bedZT10_r1_bw_split_peaks.bed ZT10_r2_bw_split_peaks.bed ZT14_r1_bw_split_peaks.bed ZT14_r2_bw_split_peaks.bed ZT18_r1_bw_split_peaks.bed ZT18_r2_bw_split_peaks.bed ZT22_r1_bw_split_peaks.bed ZT22_r2_bw_split_peaks.bed > ALL_split_C.bed
 
 #hier manuell sites rausnehmen, die nur 1x auftauchen!
 
