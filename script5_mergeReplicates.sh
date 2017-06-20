d278ae8a2952a0cc79c1ec02e0a124e1de95b342
Lars Knopf <lars.knopf@gmx.de>
2017-06-20 10:15:18 +0200

    changed file names

--- script5_mergeReplicates.sh
+++ script5_mergeReplicates.sh
@@ -3,44 +3,27 @@
 
 
 #for normal peaks
-mergePeaks -d given 150 ZT02_r1_bw_split_peaks.bed ZT02_r2_bw_split_peaks.bed > ZT02_split_C.bed
-mergePeaks -d given 150 ZT06_r1_bw_split_peaks.bed ZT06_r2_bw_split_peaks.bed > ZT06_split_C.bed
-mergePeaks -d given 150 ZT10_r1_bw_split_peaks.bed ZT10_r2_bw_split_peaks.bed > ZT10_split_C.bed
-mergePeaks -d given 150 ZT14_r1_bw_split_peaks.bed ZT14_r2_bw_split_peaks.bed > ZT14_split_C.bed
-mergePeaks -d given 150 ZT18_r1_bw_split_peaks.bed ZT18_r2_bw_split_peaks.bed > ZT18_split_C.bed
-mergePeaks -d given 150 ZT22_r1_bw_split_peaks.bed ZT22_r2_bw_split_peaks.bed > ZT22_split_C.bed
+#mergePeaks -d given 150 ZT02_r1_bw_split_peaks.bed ZT02_r2_bw_split_peaks.bed > ZT02_split_C.bed
+#mergePeaks -d given 150 ZT06_r1_bw_split_peaks.bed ZT06_r2_bw_split_peaks.bed > ZT06_split_C.bed
+#mergePeaks -d given 150 ZT10_r1_bw_split_peaks.bed ZT10_r2_bw_split_peaks.bed > ZT10_split_C.bed
+#mergePeaks -d given 150 ZT14_r1_bw_split_peaks.bed ZT14_r2_bw_split_peaks.bed > ZT14_split_C.bed
+#mergePeaks -d given 150 ZT18_r1_bw_split_peaks.bed ZT18_r2_bw_split_peaks.bed > ZT18_split_C.bed
+#mergePeaks -d given 150 ZT22_r1_bw_split_peaks.bed ZT22_r2_bw_split_peaks.bed > ZT22_split_C.bed
 
 #for subpeaks
 
-mergePeaks -d given 150 ZT02_r1_bw_split_peaks.subpeaks.bed ZT02_r2_bw_split_peaks.subpeaks.bed > ZT02_split_C.subpeaks.bed
-mergePeaks -d given 150 ZT06_r1_bw_split_peaks.subpeaks.bed ZT06_r2_bw_split_peaks.subpeaks.bed > ZT06_split_C.subpeaks.bed
-mergePeaks -d given 150 ZT10_r1_bw_split_peaks.subpeaks.bed ZT10_r2_bw_split_peaks.subpeaks.bed > ZT10_split_C.subpeaks.bed
-mergePeaks -d given 150 ZT14_r1_bw_split_peaks.subpeaks.bed ZT14_r2_bw_split_peaks.subpeaks.bed > ZT14_split_C.subpeaks.bed
-mergePeaks -d given 150 ZT18_r1_bw_split_peaks.subpeaks.bed ZT18_r2_bw_split_peaks.subpeaks.bed > ZT18_split_C.subpeaks.bed
-mergePeaks -d given 150 ZT22_r1_bw_split_peaks.subpeaks.bed ZT22_r2_bw_split_peaks.subpeaks.bed > ZT22_split_C.subpeaks.bed
-
-#merge complete set
-mergePeaks -d given 150 ZT02_r1_bw_split_peaks.bed ZT02_r2_bw_split_peaks.bed ZT06_r1_bw_split_peaks.bed ZT06_r2_bw_split_peaks.bed > ZT06_split_C.bedZT10_r1_bw_split_peaks.bed ZT10_r2_bw_split_peaks.bed ZT14_r1_bw_split_peaks.bed ZT14_r2_bw_split_peaks.bed ZT18_r1_bw_split_peaks.bed ZT18_r2_bw_split_peaks.bed ZT22_r1_bw_split_peaks.bed ZT22_r2_bw_split_peaks.bed > ALL_split_C.bed
-
-#hier manuell sites rausnehmen, die nur 1x auftauchen!
+#mergePeaks -d given 150 ZT02_r1_bw_split_peaks.subpeaks.bed ZT02_r2_bw_split_peaks.subpeaks.bed > ZT02_split_C.subpeaks.bed
+#mergePeaks -d given 150 ZT06_r1_bw_split_peaks.subpeaks.bed ZT06_r2_bw_split_peaks.subpeaks.bed > ZT06_split_C.subpeaks.bed
+#mergePeaks -d given 150 ZT10_r1_bw_split_peaks.subpeaks.bed ZT10_r2_bw_split_peaks.subpeaks.bed > ZT10_split_C.subpeaks.bed
+#mergePeaks -d given 150 ZT14_r1_bw_split_peaks.subpeaks.bed ZT14_r2_bw_split_peaks.subpeaks.bed > ZT14_split_C.subpeaks.bed
+#mergePeaks -d given 150 ZT18_r1_bw_split_peaks.subpeaks.bed ZT18_r2_bw_split_peaks.subpeaks.bed > ZT18_split_C.subpeaks.bed
+#mergePeaks -d given 150 ZT22_r1_bw_split_peaks.subpeaks.bed ZT22_r2_bw_split_peaks.subpeaks.bed > ZT22_split_C.subpeaks.bed
 
 #mergePeaks -d 150 ZT02_C.bed ZT06_C.bed ZT10_C.bed ZT14_C.bed ZT18_C.bed ZT22_C.bed > allmerged_C.txt
 
-#hier auch manuell
+#merge complete set
+mergePeaks -d given 150 ZT02_r1_bw_split_peaks.bed ZT02_r2_bw_split_peaks.bed ZT06_r1_bw_split_peaks.bed ZT06_r2_bw_split_peaks.bed ZT10_r1_bw_split_peaks.bed ZT10_r2_bw_split_peaks.bed ZT14_r1_bw_split_peaks.bed ZT14_r2_bw_split_peaks.bed ZT18_r1_bw_split_peaks.bed ZT18_r2_bw_split_peaks.bed ZT22_r1_bw_split_peaks.bed ZT22_r2_bw_split_peaks.bed > ALL_split_C.bed
+
+#now use filter_peaks.R to select peaks for appearance threshold
 
 
-"
-mergePeaks -d given 500 ZT02_r1_bw_peaks.bed ZT02_r2_bw_peaks.bed > ZT02_D.bed
-mergePeaks -d given 500 ZT06_r1_bw_peaks.bed ZT06_r2_bw_peaks.bed > ZT06_D.bed
-mergePeaks -d given 500 ZT10_r1_bw_peaks.bed ZT10_r2_bw_peaks.bed > ZT10_D.bed
-mergePeaks -d given 500 ZT14_r1_bw_peaks.bed ZT14_r2_bw_peaks.bed > ZT14_D.bed
-mergePeaks -d given 500 ZT18_r1_bw_peaks.bed ZT18_r2_bw_peaks.bed > ZT18_D.bed
-mergePeaks -d given 500 ZT22_r1_bw_peaks.bed ZT22_r2_bw_peaks.bed > ZT22_D.bed
-
-mergePeaks -d 500 ZT02_r1_bw_peaks.bed ZT02_r2_bw_peaks.bed > ZT02_E.bed
-mergePeaks -d 500 ZT06_r1_bw_peaks.bed ZT06_r2_bw_peaks.bed > ZT06_E.bed
-mergePeaks -d 500 ZT10_r1_bw_peaks.bed ZT10_r2_bw_peaks.bed > ZT10_E.bed
-mergePeaks -d 500 ZT14_r1_bw_peaks.bed ZT14_r2_bw_peaks.bed > ZT14_E.bed
-mergePeaks -d 500 ZT18_r1_bw_peaks.bed ZT18_r2_bw_peaks.bed > ZT18_E.bed
-mergePeaks -d 500 ZT22_r1_bw_peaks.bed ZT22_r2_bw_peaks.bed > ZT22_E.bed
-"
